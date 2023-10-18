d9145e2ad8090cdcf12cc355319db403142754d7
TatendaTorerwa <ttorerwa01@gmail.com>
2023-10-18 06:49:17 -0700

    is prints nothing.

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+  *nop- Prints nothing.
+  *
+  *@head: Top of the stack.
+  *
+  *@number: line_number.
+  *
+  *Return: no return
+ */
+void nop(stack_t **head, unsigned int number)
+{
+	(void) number;
+	(void) head;
+}
