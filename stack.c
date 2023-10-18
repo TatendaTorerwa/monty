a1ab9241016d9d5711b5863ca73faf4d3c00a82e
TatendaTorerwa <ttorerwa01@gmail.com>
2023-10-18 08:01:47 -0700

    LIFO.

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * stack - Prints the top.
+ * @head: Top of the stack.
+ * @number: line_number
+ * Return: no return
+*/
+void stack(stack_t **head, unsigned int number)
+{
+	(void)head;
+	(void)number;
+	bus.lifi = 0;
+}
